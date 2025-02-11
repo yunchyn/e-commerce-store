8487f0507e6480c805c174ddccd5cdf53c9a10e0
unknown <doeh2580@gmail.com>
2025-02-11 17:45:19 +0900

    [FEAT] user 페이지 카테고리별 컴포넌트를 보여주는 기능 추가, user 페이지 반응형 작업(진행중), user 페이지 모바일 카테고리 드롭다운 적용

--- components/user/WishList.tsx
+++ components/user/WishList.tsx
@@ -2,15 +2,17 @@
 
 import { supabase } from "@/supabase";
 import { useRouter } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { fetchWishlistByMemberId, IWishProduct } from "../dataHandler";
 import WishItem from "./WishItem";
 import { WishItemSkeleton } from "../SkeletonComponents";
+import { SessionContext } from "../SessionProvider";
 
-export default function WishList({ userId }: { userId: string }) {
+export default function WishList() {
   const router = useRouter();
   const [wishProducts, setWishProducts] = useState<IWishProduct[]>([]);
   const [loading, setLoading] = useState(true);
+  const userSession = useContext(SessionContext);
 
   const handleRemove = async (wishlistId: number) => {
     const { error } = await supabase.from("wishlist").delete().match({ wishlist_id: wishlistId });
@@ -25,17 +27,15 @@ export default function WishList({ userId }: { userId: string }) {
 
   useEffect(() => {
     async function fetchSessionAndLoadWish() {
-      const {
-        data: { session },
-      } = await supabase.auth.getSession();
-      if (!session) {
+      if (!userSession) return;
+
+      if (!userSession.userId) {
         alert("Login is required.");
         router.push("/auth");
         return;
       }
-      const userId = session.user.id;
 
-      const fetchedProduct = await fetchWishlistByMemberId(userId);
+      const fetchedProduct = await fetchWishlistByMemberId(userSession.userId);
       if (fetchedProduct) {
         console.log(fetchedProduct);
         setWishProducts(fetchedProduct);
@@ -47,33 +47,31 @@ export default function WishList({ userId }: { userId: string }) {
   }, [router]);
 
   return (
-    <div className="px-[72px] w-3/4">
-      <div className="flex flex-col">
-        <div className="text-body1Semi font-[32px] pb-10">Your Wishlist</div>
-        {/* 리스트 */}
-        <div
-          className="grid grid-cols-3 text-neutral-4 text-caption1 font-caption w-full text-center
+    <div className="flex flex-col">
+      <div className="text-body1Semi font-[32px] pb-10">Your Wishlist</div>
+      {/* 리스트 */}
+      <div
+        className="grid grid-cols-3 text-neutral-4 text-caption1 font-caption w-full text-center
         border-b border-neutral-3 pb-2"
-        >
-          <p className="text-left">Product</p>
-          <p>Price</p>
-          <p>Action</p>
-        </div>
-        <div>
-          {loading ? (
-            Array.from({ length: 3 }).map((_, index) => <WishItemSkeleton key={index} />)
-          ) : wishProducts.length === 0 ? (
-            <div className="text-center py-48 text-body2 font-body text-neutral-4">Your wishlist is empty.</div>
-          ) : (
-            wishProducts.map((product) => (
-              <WishItem
-                key={product.wishlist_id}
-                product={product}
-                handleRemove={handleRemove}
-              />
-            ))
-          )}
-        </div>
+      >
+        <p className="text-left">Product</p>
+        <p className="max-sm:hidden">Price</p>
+        <p className="max-sm:hidden">Action</p>
+      </div>
+      <div>
+        {loading ? (
+          Array.from({ length: 3 }).map((_, index) => <WishItemSkeleton key={index} />)
+        ) : wishProducts.length === 0 ? (
+          <div className="text-center py-48 text-body2 font-body text-neutral-4">Your wishlist is empty.</div>
+        ) : (
+          wishProducts.map((product) => (
+            <WishItem
+              key={product.wishlist_id}
+              product={product}
+              handleRemove={handleRemove}
+            />
+          ))
+        )}
       </div>
     </div>
   );
